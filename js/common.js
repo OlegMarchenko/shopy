ca6f7d132c105261c8adcaa07aae16b9176d42b7
OlegMarchenko <workolegmarchenko@gmail.com>
2017-07-20 14:06:57 +0300

    finished prodacts-page

--- js/common.js
+++ js/common.js
@@ -6,10 +6,22 @@ $(document).ready(function() {
             }
         });
     })
-    $('.main-section').slick({
-        dots: true,
-        vertical: true,
-        verticalSwiping: true,
-        appendDots: '.appendDots'
+
+    if($('.main-section').length) {
+        $('.main-section').slick({
+            dots: true,
+            vertical: true,
+            verticalSwiping: true,
+            appendDots: '.appendDots'
+        });
+    }
+
+    $('.range-slider').jRange({
+        from: 0,
+        to: 1100,
+        isRange : true,
+        width: 186
     });
+
+    $('.range-slider').jRange('updateRange', '0,1100', '100,1000');
 });
