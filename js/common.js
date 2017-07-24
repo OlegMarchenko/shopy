6a0fb45334d8ac99de92af932387463c5b82f6d0
OlegMarchenko <workolegmarchenko@gmail.com>
2017-07-24 13:22:32 +0300

    dd

--- js/common.js
+++ js/common.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
             verticalSwiping: true,
             infinite: false,
             mobileFirst: true,
+            touchMove: false,
             appendDots: '.appendDots'
         });
     }
