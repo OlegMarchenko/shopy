643024070ed2eb8e60feeb4b5f112d8e0fdd465e
OlegMarchenko <workolegmarchenko@gmail.com>
2017-07-27 13:29:21 +0300

    responsive for products page

--- js/common.js
+++ js/common.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
 
+    $(".navbar-toggler").click(function () {
+        $(".navbar-nav").slideToggle(function () {
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+    })
+    if ($('.main-section').length) {
+        $('.main-section').slick({
+            dots: true,
+            vertical: true,
+            verticalSwiping: true,
+            infinite: false,
+            mobileFirst: true,
+            appendDots: '.appendDots'
+        });
+    }
+
+    if ($('.range-slider').length) {
+        $('.range-slider').jRange({
+            from: 0,
+            to: 1100,
+            isRange: true,
+            width: 186
+        });
+        $('.range-slider').jRange('updateRange', '0,1100', '100,1000');
+
+        $('#from').on('keyup', function (event) {
+            var minPrice = event.currentTarget.value;
+            $('.range-slider').jRange('setValue', minPrice+','+'555');
+        })
+    }
+
+
+
     if ($('.top-slider-init').length) {
         $('.top-slider-init').slick({
             dots: false,
@@ -21,35 +56,6 @@ $(document).ready(function () {
             autoplay:true
         });
     }
-
-
-    $(".navbar-toggler").click(function () {
-        $(".navbar-nav").slideToggle(function () {
-            if ($(this).css('display') === 'none') {
-                $(this).removeAttr('style');
-            }
-        });
-    })
-    if ($('.main-section').length) {
-        $('.main-section').slick({
-            dots: true,
-            vertical: true,
-            verticalSwiping: true,
-            infinite: false,
-            mobileFirst: true,
-            appendDots: '.appendDots'
-        });
-    }
-
-
-    $('.range-slider').jRange({
-        from: 0,
-        to: 1100,
-        isRange: true,
-        width: 186
-    });
-
-    $('.range-slider').jRange('updateRange', '0,1100', '100,1000');
 
 
 });
